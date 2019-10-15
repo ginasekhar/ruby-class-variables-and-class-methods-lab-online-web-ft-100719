a33b722b8e977a9f6c552c4529e5230ed43658ec
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-15 12:23:10 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+class Song 
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0 
+  @@genres = []
+  @@artists = []
+  
+  def initialize
+    @@count += 1
+    @@genres << @genres
+    @@artists << @artists
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres 
+    @@genres.uniq
+  end
+  
+  def self.artists 
+    @@artists.uniq
+  end
+end
